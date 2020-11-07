0b089b8e603486c395ab3ed788cbee4b1390516d
Chris Lyko <chrislyko@gmail.com>
2020-11-07 00:46:20 -0500

    last push of the night, added lots of functionality

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var playBtn = document.querySelector("#playBtn");
 var viewHs1 = document.querySelector(".viewHs");
 var viewHs2 = document.querySelector("#viewHs");
 var timer = document.querySelector(".timer");
+var QBank = document.querySelector("#current-question-bank");
 
 var interval;
+var questionIndex;
 var stillPlaying = false;
 var timerVal = 100;
 timer.textContent = timerVal;
@@ -19,11 +21,26 @@ let questions = [
         answers: ["bare", "too many loops", "zero", "void"],
         correctIDX: 2
     },
+    {
+        question: "When do while loops stop?",
+        answers: ["when the global variables are deleted", "when you close the browser", "when DOMS collide", "when the condition is false"],
+        correctIDX: 3
+    },
+    {
+        question: "How great is Apple?",
+        answers: ["it's horrible", "same phone just more cameras", "why would you ever buy from them", "all of the above"],
+        correctIDX: 3
+    },
+    {
+        question: "How hard is coding?",
+        answers: ["its easyyy", "never give up", "no one can do it", "better off being a lawyer"],
+        correctIDX: 2
+    },
 ]
 
 function init(){
     timerVal = 100;
-    console.log(JSON.stringify(questions[1].question))
+    questionIndex = 0;
 }
 
 function startGame() {
@@ -31,6 +48,8 @@ function startGame() {
     viewHs1.style.visibility = "hidden";
     viewHs2.style.visibility = "hidden";
     stillPlaying = true;
+
+    renderQuestion();
 
     interval = setInterval(function () {
         if (stillPlaying) {
@@ -44,6 +63,40 @@ function startGame() {
           timer.textContent = timerVal;
         }
       }, 1000);
+
+    
+}
+
+function renderQuestion() {
+    var ques = document.createElement("h2");
+    ques.textContent = JSON.stringify(questions[questionIndex].question);
+    QBank.appendChild(ques);
+
+    
+    for(let i=0; i < questions[questionIndex].answers.length; i++){
+        var li = document.createElement("li");
+        var answers = document.createElement("button");
+        answers.textContent = JSON.stringify(questions[questionIndex].answers[i]);
+        answers.setAttribute("data-index", i)
+
+        QBank.appendChild(li);
+        li.appendChild(answers);
+    }
+
+    QBank.addEventListener("click", function(event){
+        var element = event.target;
+        if(element.matches("button")){
+            var index = element.getAttribute("data-index");
+            if(index == JSON.stringify(questions[questionIndex].correctIDX)){
+                //correct
+                questionIndex++;
+                renderQuestion();
+            }else {
+                //wrong
+                timerVal -= 20;
+            }
+        }
+    });
 }
 
 function gameOver(){}
